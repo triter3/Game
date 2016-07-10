a1496dd5676c77e0b92eb0c73b36b1eb675aac3e
triter3 <playtriter3@gmail.com>
2016-07-10 13:31:24 +0200

    update game tools

--- core/src/com/game/Main.java
+++ core/src/com/game/Main.java
@@ -3,17 +3,18 @@ package com.game;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Main implements ApplicationListener {
 	SpriteBatch batch;
-	Texture img;
+	OrthographicCamera camera;
+	World world;
+	
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
@@ -21,7 +22,7 @@ public class Main implements ApplicationListener {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		
 		batch.end();
 	}
 
--- core/src/com/game/World.java
+++ core/src/com/game/World.java
@@ -1,5 +1,23 @@
 package com.game;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public class World {
+	
+	public World(){
+		
+	}
+	
+	public void init(){
+		
+	}
+	
+	public void update(float deltaTime){
+		
+	}
+	
+	public void render(SpriteBatch batch){
+		
+	}
 
 }
